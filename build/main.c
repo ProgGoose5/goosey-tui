f5a3238c2a1bb993fcd50bc90ad0e3a018f8363d
Goosely! <wwgoose5@gmail.com>
2024-12-28 02:57:29 +0000

    Updating main.c, now with colors
    Now both arrays save themself separatly, also added simple colors.

--- build/main.c
+++ build/main.c
@@ -1,54 +1,78 @@
 #include "variables.h"
 
+#define UNSELECTED_PAIR 1
+#define SELECTED_PAIR 2
+
 int actcaj = 0;
 int cajas = 2;
 char* apps[]={ "firefox", "terminology" };
 char* appsexe[]={ "firefox", "exec terminology" };
 
 void Loading(char* local_apps[], char* local_appsexe[]) {
+    attroff(COLOR_PAIR(UNSELECTED_PAIR)); 
+    attroff(COLOR_PAIR(SELECTED_PAIR)); 
     FILE* file = fopen("apps.txt", "r");
+    FILE* file2 = fopen("executables.txt", "r");
     if (file == NULL) {
         return;
     }
+       if (file2 == NULL) {
+        return;
+    }
+
 
     fscanf(file, "%d", &cajas);
     fscanf(file, "%d", &actcaj);
 
     for (int i = 0; i < cajas; i++) {
-        char app[50];
+        char app[100];
         fscanf(file, "%s", app);
         local_apps[i] = strdup(app);
     }
 
     for (int i = 0; i < cajas; i++) {
-        char app[50];
-        fscanf(file, "%s", app);
+        char app[100];
+        fscanf(file2, "%s", app);
         local_appsexe[i] = strdup(app);
     }
     fclose(file);
+    fclose(file2);
 }
 
 void Saving(char* local_apps[], char* local_appsexe[]) {
+     attroff(COLOR_PAIR(UNSELECTED_PAIR)); 
+    attroff(COLOR_PAIR(SELECTED_PAIR)); 
     FILE* file = fopen("apps.txt", "w");
+    FILE* file2 = fopen("executables.txt", "w");
     if (file == NULL) {
+        return;
+    }
+    if (file2 == NULL) {
         return;
     }
 
     fprintf(file, "%d\n", cajas);
     fprintf(file, "%d\n", actcaj);
+ 
+    fprintf(file2, "%d\n", cajas);
+    fprintf(file2, "%d\n", actcaj);
 
     for (int i = 0; i < cajas; i++) {
         fprintf(file, "%s\n", local_apps[i]);
     }
 
     for (int i = 0; i < cajas; i++) {
-        fprintf(file, "%s\n", local_appsexe[i]);
+        fprintf(file2, "%s\n", local_appsexe[i]);
     }
     fclose(file);
+    fclose(file2);
 }
 
 int main() {
     initscr();
+    start_color();
+    init_pair(UNSELECTED_PAIR, COLOR_WHITE, COLOR_BLACK);
+    init_pair(SELECTED_PAIR, COLOR_BLACK, COLOR_WHITE);
     keypad(stdscr, TRUE);
     noecho(); 
 
@@ -57,15 +81,23 @@ int main() {
 
     while (1) {
         clear();
+        attron(COLOR_PAIR(UNSELECTED_PAIR));
         printw("TUI Bar \n"); 
 
         for ( int i = 0; i < cajas; i++) {
-            printw("%s", apps[i]);
+            
             if (i == actcaj) {
-                printw("<< \t ");
+               attron(COLOR_PAIR(SELECTED_PAIR));
+              
             } else {
-                printw("\t \t");
+                attron(COLOR_PAIR(UNSELECTED_PAIR));
+                
             }
+          
+            printw("%s \t ", apps[i]);
+            
+            
+            
         }
 
         refresh();
@@ -85,21 +117,23 @@ int main() {
                 break;
 
             case KEY_UP:
+            attron(COLOR_PAIR(UNSELECTED_PAIR));
                 clear();
                 printw("Write down Application Name: ");
-                char new_app[50];
+                char new_app[100];
                 echo();
                 getstr(new_app);
                 noecho();
                 apps[actcaj + 1] = strdup(new_app);
 
                 printw("Write down the executable: ");
-                char new_app2[50];
+                char new_app2[100];
                 echo();
                 getstr(new_app2);
                 noecho();
                 appsexe[actcaj + 1] = strdup(new_app2);
                 cajas++;
+                attron(COLOR_PAIR(UNSELECTED_PAIR));
                 Saving(apps, appsexe); 
 
 
