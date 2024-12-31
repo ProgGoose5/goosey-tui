1b06fa5a87a0f731bfa81e0090dee4b15e4e16fb
Goosely! <wwgoose5@gmail.com>
2024-12-31 05:29:20 +0000

    Updating variables.h with new declarations.

--- variables.h
+++ variables.h
@@ -1,19 +1,27 @@
 #ifndef VARIABLES_H
 #define VARIABLES_H
 
+//We define the variables, the libs and the fuction that we will use in the program.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <ncurses.h>
 #include <string.h>
+#include "declarations.c"
+#include <utf8/utf8.h>
 
+//Variables
 extern int actcaj;
 extern int cajas;
 extern char* apps[];
 extern char* appsexe[];
+extern int apps_layout;
 
-extern void Saving(char* apps[], char* appsexe[]);
-extern void Loading(char* apps[], char* appsexe[]);
-extern void ClearSaveFiles(char* apps[], char* appsexe[]);
+//Functions
+extern void Saving();
+extern void Loading();
+extern void ClearSaveFiles();
+extern void Upperdecoration();
+extern void KeyCommands();
 #endif
